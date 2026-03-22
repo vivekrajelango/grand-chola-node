2a855d0214367dadc5c7c18388622c88c72f4b37
vivek-gitacloud <vivek@gitacloud.com>
2026-03-22 14:25:12 +0000

    order-fixes

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const cron = require("node-cron");
 const restaurantServices = require("./src/services/Restaurant.services")
 
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }))
 app.use(cors())
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false, limit: '10mb' }))
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
